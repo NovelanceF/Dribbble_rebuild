73fa0bc8b00c3c42f5109ca937e730368e0072a7
Novelance <Novelancef@gmail.com>
2014-01-26 01:22:55 +0800

    what

--- Dribbb/src/main/java/com/lance/dribbb/activites/ContentActivity.java
+++ Dribbb/src/main/java/com/lance/dribbb/activites/ContentActivity.java
@@ -13,11 +13,9 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.TextView;
 
 import com.astuetz.PagerSlidingTabStrip;
@@ -28,7 +26,7 @@ import com.lance.dribbb.fragment.shots.ShotsFragment;
 import com.lance.dribbb.network.DribbbleAPI;
 
 public class ContentActivity extends FragmentActivity implements OnPageChangeListener {
-  
+
   private ViewPager contentPager;
   private ActionBarDrawerToggle mDrawerToggle;
   private DrawerLayout mDrawerLayout;
@@ -39,60 +37,60 @@ public class ContentActivity extends FragmentActivity implements OnPageChangeLis
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_content);
-    
+
     setPager();
     setActionBarStyle();
-    
-    mDrawerLayout = (DrawerLayout)findViewById(R.id.content_drawer);
+
+    mDrawerLayout = (DrawerLayout) findViewById(R.id.content_drawer);
     mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-    mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 
-        R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+    mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+            R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
 
       public void onDrawerClosed(View view) {
-        
+
       }
 
       public void onDrawerOpened(View drawerView) {
-        
+
       }
     };
-    
+
     mDrawerLayout.setDrawerListener(mDrawerToggle);
-    
+
     getSupportFragmentManager().beginTransaction()
-        .replace(R.id.left_drawer, new Drawer(ContentActivity.this))
-        .commit();
-    
+            .replace(R.id.left_drawer, new Drawer(ContentActivity.this))
+            .commit();
+
   }
-  
+
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-      getMenuInflater().inflate(R.menu.content, menu);
-      return true;
+    getMenuInflater().inflate(R.menu.content, menu);
+    return true;
   }
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-      int id = item.getItemId();
-      if (id == R.id.action_settings) {
-        contentPager.setAdapter(adapter);
-        return true;
-      }
+    int id = item.getItemId();
+    if (id == R.id.action_settings) {
+      contentPager.setAdapter(adapter);
+      return true;
+    }
 
-      if (mDrawerToggle.onOptionsItemSelected(item)) {
-        return true;
-      }
-      return super.onOptionsItemSelected(item);
+    if (mDrawerToggle.onOptionsItemSelected(item)) {
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
-  
+
   @Override
   protected void onPostCreate(Bundle savedInstanceState) {
-      super.onPostCreate(savedInstanceState);
-      mDrawerToggle.syncState();
+    super.onPostCreate(savedInstanceState);
+    mDrawerToggle.syncState();
   }
-  
+
   private void setPager() {
-    contentPager = (ViewPager)findViewById(R.id.content_pager);
+    contentPager = (ViewPager) findViewById(R.id.content_pager);
     contentPager.setOnPageChangeListener(this);
     adapter = new mPagerAdapter(getSupportFragmentManager());
     contentPager.setAdapter(adapter);
@@ -101,9 +99,9 @@ public class ContentActivity extends FragmentActivity implements OnPageChangeLis
     tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
     tabs.setViewPager(contentPager);
   }
-  
+
   private class mPagerAdapter extends FragmentStatePagerAdapter {
-    
+
     private String Title[] = {"      DEBUT     ", "   POPULAR  ", "   EVERYONE   "};
 
     public mPagerAdapter(FragmentManager fm) {
@@ -120,41 +118,41 @@ public class ContentActivity extends FragmentActivity implements OnPageChangeLis
     public int getCount() {
       return 3;
     }
-    
+
     @Override
     public CharSequence getPageTitle(int position) {
       return Title[position];
     }
-    
+
   }
 
   @Override
   public void onPageScrollStateChanged(int arg0) {
-    
+
   }
 
   @Override
   public void onPageScrolled(int arg0, float arg1, int arg2) {
-    
+
   }
 
   @Override
   public void onPageSelected(int position) {
     getActionBar().setSelectedNavigationItem(position);
   }
-  
+
   private void setActionBarStyle() {
-    this.getActionBar().setTitle("dribbble");    
+    this.getActionBar().setTitle("dribbble");
     getActionBar().setBackgroundDrawable(this.getBaseContext().getResources().getDrawable(R.drawable.actionbar_back));
     getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
     int titleId = Resources.getSystem().getIdentifier("action_bar_title", "id", "android");
     TextView textView = (TextView) findViewById(titleId);
     textView.setTypeface(Typeface.createFromAsset(getAssets(), "font/Wendy.ttf"));
     textView.setTextColor(0xFFdfdfdf);
-    textView.setTextSize(32); 
+    textView.setTextSize(32);
     getActionBar().setDisplayHomeAsUpEnabled(true);
     getActionBar().setHomeButtonEnabled(true);
-    
+
   }
 
 }
